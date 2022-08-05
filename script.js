62fb388e76c4813af68f2a3ed567150d3b02b0d4
LsHenrique <developerluishenrique@gmail.com>
2022-08-05 12:35:27 -0300

    Versao2
    Inseridos as alterações que já haviam
    sido feitas ontem e mais adições de
    hoje.

---
+++ script.js
@@ -0,0 +1,47 @@
+function esconder1(){
+    var ocultar = document.getElementById("ocult1").style.display;
+    if(ocultar == "none"){
+        document.getElementById("ocult1").style.display = "flex";
+    }else
+        document.getElementById("ocult1").style.display = "none";
+}
+
+function esconder2(){
+    var ocultar = document.getElementById("ocult2").style.display;
+    if(ocultar == "none"){
+        document.getElementById("ocult2").style.display = "flex";
+    }else
+        document.getElementById("ocult2").style.display = "none";
+}
+
+function esconder3(){
+    var ocultar = document.getElementById("ocult3").style.display;
+    if(ocultar == "none"){
+        document.getElementById("ocult3").style.display = "flex";
+    }else
+        document.getElementById("ocult3").style.display = "none";
+}
+
+function esconder4(){
+    var ocultar = document.getElementById("ocult4").style.display;
+    if(ocultar == "none"){
+        document.getElementById("ocult4").style.display = "flex";
+    }else
+        document.getElementById("ocult4").style.display = "none";
+}
+
+function esconder5(){
+    var ocultar = document.getElementById("ocult5").style.display;
+    if(ocultar == "none"){
+        document.getElementById("ocult5").style.display = "flex";
+    }else
+        document.getElementById("ocult5").style.display = "none";
+}
+
+function esconder6(){
+    var ocultar = document.getElementById("ocult6").style.display;
+    if(ocultar == "none"){
+        document.getElementById("ocult6").style.display = "flex";
+    }else
+        document.getElementById("ocult6").style.display = "none";
+}
